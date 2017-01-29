f1f9bc6e88266c2c80894a17c75ff23890d39c77
Rufian55 <kearnsc@oregonstate.edu>
2017-01-29 13:09:41 -0500

    3

---
+++ Puppy.java
@@ -0,0 +1,12 @@
+public class Puppy {
+	public Puppy(String name) {
+		// This constructor has 1 parameter, name.
+		System.out.println("Passed Name is: " + name);
+	}
+		
+	public static void main(String []args) {
+		// Instantiates a new Puppy object.
+		Puppy myPuppy = new Puppy("Sic_em!");
+	}
+}
+
