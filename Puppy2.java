ebfe82ed2c677b9391c2489e4ab54f9ac4a9926d
Rufian55 <kearnsc@oregonstate.edu>
2017-01-29 13:51:27 -0500

    Puppy maker

---
+++ Puppy2.java
@@ -0,0 +1,35 @@
+public class Puppy2 {
+	int puppyAge;
+	String puppyName;
+
+	public void setName(String name) {
+		puppyName = name;
+	}
+
+	public String getName(){
+		return puppyName;		
+	}
+	
+	public void setAge(int age){
+		puppyAge = age;
+	}
+	
+	public int getAge(){
+		return puppyAge;
+	}
+		
+	public static void main(String []args) {
+		// Instantiates a new Puppy object.
+		Puppy2 myPuppy = new Puppy2();
+
+		// Call class methoid to set puppy's name.
+		myPuppy.setName("Sic_em!");		
+
+		// Call class method to set puppy's age.
+		myPuppy.setAge(69);
+		
+		// Print instance vars.
+		System.out.println("The Puppy's name is: " + myPuppy.getName());
+		System.out.println("The Puppy's Age is: " + myPuppy.getAge());
+	}
+}
