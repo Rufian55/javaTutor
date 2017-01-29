ec653e609456d25a07e383080984960d72b1adc5
Rufian55 <kearnsc@oregonstate.edu>
2017-01-29 12:31:03 -0500

    Hello yourself!

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,12 @@
+public class MyFirstJavaProgram {
+	
+	/* This is my very first java program, well
+	 * acctually my second...
+	 * I like this eclips IDE...*/
+
+	public static void main(String []args) {
+		// Prints the ubiquitous Hello World string.
+		System.out.println("Hello World");
+	}
+}
+
