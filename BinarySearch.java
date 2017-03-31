2060d1d3d9d399e44fc12c8c4626bf65328428e0
Rufian55 <kearnsc@oregonstate.edu>
2017-03-31 18:59:41 -0400

    Today's stuff

--- BinarySearch.java
+++ BinarySearch.java
@@ -10,7 +10,7 @@ public class BinarySearch {
 		int anArray[] = {2, 4, 3, 0, 7, 1, -4, 99, 69};
 		
 		// Show sorted anArray.
-		System.out.print("anArry unsorted = ");
+		System.out.print("anArray unsorted = ");
 		for(int num : anArray){
 			System.out.print(num + " ");
 		}
@@ -21,16 +21,28 @@ public class BinarySearch {
 		Arrays.sort(anArray);
 		
 		// Show sorted anArray.
-		System.out.print("anArry sorted = ");
+		System.out.print("anArray sorted = ");
 		for(int num : anArray){
 			System.out.print(num + " ");
 		}
 	
-		int seekThis = 0;
+		int seekThis = -1, seekThat = 7;
 		
 		int sought4 = Arrays.binarySearch(anArray, seekThis);
+		int alsoSought4 = Arrays.binarySearch(anArray, seekThat); 
 	
-		System.out.println("\n" + seekThis + " was found at anArray[" + sought4 + "]");
-		
+		if(sought4 < 0)// If seekThis is negative, it's the bitwise not of insert the point.
+			System.out.println("\nSought value \"" + seekThis + "\" not found.\n"
+					+ "Insertion point is anArray[" + (-(sought4)-1) + "]");
+		else
+			System.out.println("\n" + seekThis + " was found at anArray[" + sought4 + "]");
+
+		if(seekThat < 0)// If seekThat is negative, it's the bitwise not of the insert the point.
+			System.out.println("\nSought value \"" + seekThat + "\" not found.\n"
+					+ "Insertion point is anArray[" + (-(alsoSought4)-1) + "]");
+		else
+			System.out.println("\n" + seekThat + " was found at anArray[" + alsoSought4 + "]");
+	
 	}
+
 }
