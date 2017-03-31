8942de71f80fa9902e9cb72513dce4a03d9065ec
Rufian55 <kearnsc@oregonstate.edu>
2017-03-31 19:02:18 -0400

    Today's stuff

--- BinarySearch.java
+++ BinarySearch.java
@@ -31,13 +31,13 @@ public class BinarySearch {
 		int sought4 = Arrays.binarySearch(anArray, seekThis);
 		int alsoSought4 = Arrays.binarySearch(anArray, seekThat); 
 	
-		if(sought4 < 0)// If seekThis is negative, it's the bitwise not of insert the point.
+		if(sought4 < 0)// If seekThis is negative, it's the bitwise not of the insert point.
 			System.out.println("\nSought value \"" + seekThis + "\" not found.\n"
 					+ "Insertion point is anArray[" + (-(sought4)-1) + "]");
 		else
 			System.out.println("\n" + seekThis + " was found at anArray[" + sought4 + "]");
 
-		if(seekThat < 0)// If seekThat is negative, it's the bitwise not of the insert the point.
+		if(seekThat < 0)// If seekThat is negative, it's the bitwise not of the insert point.
 			System.out.println("\nSought value \"" + seekThat + "\" not found.\n"
 					+ "Insertion point is anArray[" + (-(alsoSought4)-1) + "]");
 		else
